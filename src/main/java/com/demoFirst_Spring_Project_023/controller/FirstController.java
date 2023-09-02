8796c3b48909fbb2bbc4c635d3590fdd8b518df4
micha <michalhezi2@gmail.com>
2023-09-02 22:12:58 +0300

    commit1

--- src/main/java/com/demoFirst_Spring_Project_023/controller/FirstController.java
+++ src/main/java/com/demoFirst_Spring_Project_023/controller/FirstController.java
@@ -1,31 +1,49 @@
 package com.demoFirst_Spring_Project_023.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.demoFirst_Spring_Project_023.model.Customer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/first")
 public class FirstController {
 
 
-    @GetMapping(value = "/hello")
-    public static String getFunction(){
-        return "Hi";
+
+
+    @GetMapping(value = "/first_get")
+    public static String firstGet(){
+        return "This is the first get";
     }
 
-    @GetMapping(value = "/return/{s}")
-    public static String returnFunction(@PathVariable String s){
-        System.out.println("STRING");
-        return s;
+    @GetMapping(value = "/second_get/{name}")
+    public static String secondGet(@PathVariable String name){
+        return "You wrote " + name;
     }
 
-    @GetMapping(value = "/return_number/{n}")
-    public static int returnFunction(@PathVariable int n){
-        System.out.println("INTEGER");
-        return n;
+    @GetMapping(value = "/third_get")
+    public static String thirdGet(@RequestParam String name){
+        return "You sent me this name " + name;
     }
+
+    @GetMapping(value = "/fourth_get")
+    public static String fourthGet(@RequestParam int id){
+        return "You sent me this id " + id;
+    }
+
+    @PostMapping(value = "first_post")
+    public static String firstPost(@RequestBody String name){
+        return "You posted this name : " + name;
+    }
+
+    @PostMapping(value = "second_post")
+    public static String secondPost(@RequestBody Customer customer){
+        return customer.toString();
+    }
+
+
+
 
 }
